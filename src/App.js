c70f65355e8094a30f8bce6971c6489bbec29ae6
Manuel <tofuah@gmail.com>
2021-09-29 13:44:56 +0100

    created function and exported UsersFunction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import UserClass from './UserClass';
+import UsersFunction from './UsersFunction';
 
 function App() {
   return (
     <div>
       <UserClass />
+
+      <UsersFunction />
 
     </div>
   );
---
+++ src/UsersFunction.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+class App extends React.Component {
+    constructor() {
+      super();
+      this.state = { users: [] };
+    }
+  
+    //Asynchronous function which gets users from server
+    async getUsers() {
+      //gets data using fetch from url
+      fetch(" https://jsonplaceholder.typicode.com/users")
+        .then((response) => {
+          //converts response to json format and returns converted data
+          return response.json();
+        })
+        .then((users) => {
+          //receives the converted data and updates the users state to the converted data
+          this.setState({ users: users });
+        });
+    }
+  
+    //gets called when the component is first mounted
+    componentDidMount() {
+      //calls getUsers method when component is first mounted
+      this.getUsers();
+    }
+  
+    componentDidUpdate() {
+      //runs when a state is updated
+      console.log("state updated");
+    }
+  
+    componentWillUnmount() {
+      //runs when a component is about to be unmounted
+      console.log("unmounting");
+    }
+  
+    render() {
+      return (
+        <>
+          {this.state.users.map((user) => {
+            return <h1 key={user.id}>{user.username}</h1>;
+                    <h1 key={user.id}>{user.email}</h1>;
+          })}
+        </>
+      );
+    }
+  }
+  
+  export default UsersFunction;
