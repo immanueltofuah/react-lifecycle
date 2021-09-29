5724c34f968f8afb5096e05f8ca75364554ddcc3
Manuel <tofuah@gmail.com>
2021-09-29 13:47:25 +0100

    changed app tp UsersFunction

--- src/UsersFunction.js
+++ src/UsersFunction.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-class App extends React.Component {
+class UsersFunction extends React.Component {
     constructor() {
       super();
       this.state = { users: [] };
