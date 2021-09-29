82a12ce7648d227dca4aa1b66efaf8f8af5940a6
Manuel <tofuah@gmail.com>
2021-09-29 13:14:20 +0100

    created class comp and function plus imports

---
+++ src/UserClass.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+class App extends React.Component{
+    constructor() {
+        super();
+        this.state = { users: []};
+    }
+    async getUsers() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response) => {
+
+            return response.json();
+        })
+        .then((users) => {
+
+            this.setstate({ users: users});
+        });
+    }
+
+    componentDidMount() {
+
+        this.getUsers();
+    }
+
+    componentDidUpdate() {
+        console.log("state updated");
+    }
+
+    componentWillUnmount() {
+
+        console.log("unmounting");
+    }
+
+    render() {
+        return(
+            <>
+            {this.state.users.map((user) => {
+                return <h1 key={user.id}>{user.name}</h1>;
+            })}
+            </>
+        );
+    }
+}
+
